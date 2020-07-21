aa80c71e2eb4b0f6313bb04a1505c73e969ea74c
Calamar <oebolanoso@unal.edu.co>
2020-07-21 01:56:03 -0500

    Fix links and good end IDK why the text is in list in some cases

--- scraper.py
+++ scraper.py
@@ -1,13 +1,39 @@
 import requests
 import lxml.html as html
+import os
+import datetime as datetime
 
 HOME_URL = 'https://www.larepublica.co'
 
 XPATH_LINK_TO_ARTICLE = '//div/h2/a/@href' #'//div[@class="V_Title"]/h2/a/@*' DO NOT WORKS
-XPATH_TITLE = '//div[@class="mb-auto"]/h2/a[@class="economiaSect"]/text()'
+XPATH_TITLE = '//div[@class="mb-auto"]/text-fill/a[@class="economiaSect"]/text()'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY  = '//div[@class="html-content"]/p[not(@class)]/text()'
-XPATH_AUTOR = '//div[@class="autorArticule"]/p/text()'
+XPATH_AUTOR = '//div[@class="autorArticle"]/p/text()'
+
+def parse_notice(link, today):
+    try:
+        response = requests.get(link)
+        if response.status_code == 200:
+            notice = response.content.decode('utf-8')
+            parsed = html.fromstring(notice)
+            try:
+                title = parsed.xpath(XPATH_TITLE)[0]
+                autor = parsed.xpath(XPATH_AUTOR)[0]
+                summary = parsed.xpath(XPATH_SUMMARY)[0]
+                body = parsed.xpath(XPATH_BODY)
+            except IndexError:
+                return
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as file:
+                file.write(f'{title}\n{autor}')
+                file.write('\n\n')
+                file.write(f'{summary}\n')
+                for p in body:
+                    file.write(f'\n{p}')
+        else:
+            raise ValueError(f'Error: {response.status_code}')
+    except ValueError as ve:
+        print(ve)
 
 def parse_home():
     try:
@@ -16,7 +42,13 @@ def parse_home():
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            print(f'Links to notices: {links_to_notices}')
+            # print(f'Links to notices: {links_to_notices}')
+            
+            today = datetime.date.today().strftime('%d-%m-%Y')
+            if not os.path.isdir(today):
+                os.mkdir(today)
+            for link in links_to_notices:
+                parse_notice(link, today)
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
